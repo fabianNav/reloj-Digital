8e86619a1d6be69a34c8cbb7f589a2ad6faa49e4
fabianNav <fn39001@gmail.com>
2024-01-31 18:12:42 -0300

    Terminamos por completo la logica y lo insertamos al DOM, ahora le daré estilo con css y puede que le ponga un modo oscuro o algo parecido

--- main.js
+++ main.js
@@ -1,54 +1,112 @@
 import './css/style.css'
+//TODO Logica de la fecha y hora
 
-
-
-
-
-
-let fecha = new Date();
-let diaNumero = fecha.getDate();
-console.log(diaNumero);
-let hora = fecha.getHours();
-console.log(hora);
-let minutos = fecha.getMinutes();
-console.log(minutos);
-let segundos = fecha.getSeconds();
-console.log(segundos);
-let diaSemana = fecha.getDay();
-console.log(diaSemana);
-switch (diaSemana) {
-  case 0:
-    diaSemana = 'Domingo'
-    break;
+const reloj = () => {
+  let fecha = new Date();
+  let diaNumero = fecha.getDate();
+  //console.log(diaNumero);
+  let hora = fecha.getHours();
+  //console.log(hora);
+  let minutos = fecha.getMinutes();
+  if (minutos < 10) {
+    minutos = `0${minutos}`;
+  }
+  //console.log(minutos);
+  let segundos = fecha.getSeconds();
+  //console.log(segundos);
+  let diaSemana = fecha.getDay();
+  //console.log(diaSemana);
+  switch (diaSemana) {
+    case 0:
+      diaSemana = 'Domingo'
+      break;
 
     case 1:
-    diaSemana = 'Lunes'
-    break;
+      diaSemana = 'Lunes'
+      break;
 
     case 2:
-    diaSemana = 'Martes'
-    break;
+      diaSemana = 'Martes'
+      break;
 
     case 3:
-    diaSemana = 'Miercoles'
-    break;
+      diaSemana = 'Miercoles'
+      break;
 
     case 4:
-    diaSemana = 'Jueves'
-    break;
+      diaSemana = 'Jueves'
+      break;
 
     case 5:
-    diaSemana = 'Viernes'
-    break;
+      diaSemana = 'Viernes'
+      break;
 
     case 6:
-    diaSemana = 'Sabado'
-    break;
+      diaSemana = 'Sabado'
+      break;
+  }
+  //console.log(diaSemana);
+  let mes = fecha.getMonth();
+  //console.log(mes);
+  switch (mes) {
+    case 0:
+      mes = 'Enero';
+      break;
+
+    case 1:
+      mes = 'Febrero';
+      break;
+
+    case 2:
+      mes = 'Marzo';
+      break;
+
+    case 3:
+      mes = 'Abril';
+      break;
+
+    case 4:
+      mes = 'Mayo';
+      break;
+
+    case 5:
+      mes = 'Junio';
+      break;
+
+    case 6:
+      mes = 'Julio';
+      break;
+
+    case 7:
+      mes = 'Agosto';
+      break;
+
+    case 8:
+      mes = 'Septiembre';
+      break;
+
+    case 9:
+      mes = 'Octubre';
+      break;
+
+    case 10:
+      mes = 'Noviembre';
+      break;
+
+    case 11:
+      mes = 'Diciembre';
+      break;
+  }
+  //console.log(mes);
+
+  //TODO Fecha en el DOM
+  let horaDOM = document.getElementById('hora');
+  horaDOM.innerHTML = `${hora}:${minutos}:${segundos}`;
+  let diaDOM = document.getElementById('calendario');
+  diaDOM.innerHTML = `${diaSemana}, ${diaNumero}, ${mes}`
 }
-console.log(diaSemana);
 
-
-
+setInterval(reloj, 1000);
 
 
 
