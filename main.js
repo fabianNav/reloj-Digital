b457e4d0cf8ea29178581df4da7a7d86307fec8c
fabianNav <fn39001@gmail.com>
2024-01-31 17:44:56 -0300

    Primero hice la logica ya que era lo menos trabajoso, ahora sigue la estructura del html

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 import './css/style.css'
+
+
+
+
+
+
+let fecha = new Date();
+let diaNumero = fecha.getDate();
+console.log(diaNumero);
+let hora = fecha.getHours();
+console.log(hora);
+let minutos = fecha.getMinutes();
+console.log(minutos);
+let segundos = fecha.getSeconds();
+console.log(segundos);
+let diaSemana = fecha.getDay();
+console.log(diaSemana);
+switch (diaSemana) {
+  case 0:
+    diaSemana = 'Domingo'
+    break;
+
+    case 1:
+    diaSemana = 'Lunes'
+    break;
+
+    case 2:
+    diaSemana = 'Martes'
+    break;
+
+    case 3:
+    diaSemana = 'Miercoles'
+    break;
+
+    case 4:
+    diaSemana = 'Jueves'
+    break;
+
+    case 5:
+    diaSemana = 'Viernes'
+    break;
+
+    case 6:
+    diaSemana = 'Sabado'
+    break;
+}
+console.log(diaSemana);
+
+
+
+
+
+
+
+
